e49bde7a9d12182f7285073b29b9be6665fe6d3e
Daniel Nidzgorski <daniel.nidzgorski@kingcounty.gov>
2018-01-08 12:43:07 -0800

    Finish static map of TSI values.

--- TSI plotting function.R
+++ TSI plotting function.R
@@ -149,30 +149,81 @@ TSI.map<-function(data,lake,year) {
               opacity = 1
     )
   
-  # # Static ggmap
-  # pad<-0.025
-  # box<-d.TSI %>%
-  #   summarize(left=min(Longitude,na.rm=T)-pad,
-  #             right=max(Longitude,na.rm=T)+pad,
-  #             bottom=min(Latitude,na.rm=T)-pad,
-  #             top=max(Latitude,na.rm=T)+pad
-  #   )
-  # 
-  # toner<-get_stamenmap(bbox=c(box$left,box$bottom,box$right,box$top),
-  #                      maptype="toner-background",
-  #                      zoom=7
-  #                      )
-  # 
-  # s<-ggmap(toner,
-  #          maprange=T,
-  #          base_layer=ggplot(data=d.TSI,aes(x=Longitude,y=Latitude,color=TSI.Chl)))+
-  #   geom_point(size=5)+
-  #   scale_color_gradient(low="lightblue",high="darkgreen")+
-  #   geom_text_repel(aes(label=Lake),color="black",size=3)
-  
-  
-  
   # Return interactive leaflet for HTML version
   l
+  
+}
+
+TSI.map.static<-function(data,year) {
+  
+  latlong<-read_csv("lakelatlong.csv",
+                    col_types=cols(
+                      Lake = col_character(),
+                      LakeName=col_character(),
+                      Latitude = col_double(),
+                      Longitude = col_double()
+                    ))
+  
+  params.list<-c("TotalPhosphorus",
+                 "ChlorophyllA",
+                 "Secchi")
+  
+  d.means<-data %>%
+    filter(Depth==1,
+           Parameter %in% params.list) %>%
+    mutate(Year=year(Date),
+           Month=month(Date)) %>%
+    filter(Year==year,
+           Month>=5,
+           Month<=10) %>%
+    group_by(Parameter,Year,Lake) %>%
+    summarize(Mean=mean(Value,na.rm=T))
+  
+  d.TSI<-d.means %>%
+    spread(key=Parameter,value=Mean) %>%
+    mutate(TSI.Sec=10*(6-(log(Secchi/0.693))),
+           TSI.Chl=10*(6-((2.04)-(0.68*log(ChlorophyllA)))/log(2)),
+           TSI.TP=10*(6-(log(48/TotalPhosphorus)/0.693)) # TP in ug/L
+    ) %>%
+    mutate(Focus=ifelse(Lake==lake,T,F)) %>%
+    left_join(latlong,by="Lake")
+
+  
+  # Static ggmap
+  pad<-0.025
+  box<-d.TSI %>%
+    summarize(left=min(Longitude,na.rm=T)-pad,
+              right=max(Longitude,na.rm=T)+pad,
+              bottom=min(Latitude,na.rm=T)-pad,
+              top=max(Latitude,na.rm=T)+pad
+    )
+
+  maptile<-get_stamenmap(bbox=c(box$left,box$bottom,box$right,box$top),
+                       maptype="toner-background"
+                       )
+  
+
+  s<-ggmap(maptile,
+           maprange=T,
+           base_layer=ggplot(data=d.TSI,aes(x=Longitude,y=Latitude,color=TSI.Chl)))+
+    geom_point(size=4)+
+    geom_label_repel(aes(label=Lake,fill=TSI.Chl),color="white",size=4)+
+    scale_color_gradient(low="cornflowerblue",high="darkgreen",name="Chlorophyll-a TSI")+
+    scale_fill_gradient(low="cornflowerblue",high="darkgreen",name="Chlorophyll-a TSI")+
+    theme(axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.justification=c(1,0), 
+          legend.direction="horizontal",
+          legend.position=c(1,0),
+          legend.box.margin=margin(t = 0, r = 0, b = 0, l = 0, unit = "pt"),
+          legend.margin=margin(t = 0, r = 0, b = 0, l = 0, unit = "pt")
+          
+    )
+  
+  ggsave(s,filename=sprintf("%s/Plots/%s-TSI map.png",year,year),width=5.25,height=8)
+  
   
 }
